bcc2d14d7599f36705c19ae5ca356a0ca1d476f5
VisheshBansal <vishesh.bansal@gmail.com>
2021-04-19 00:17:30 +0530

    fix: reverted back to pytesseract with heuristics

---
+++ utils/difFixer.py
@@ -0,0 +1,62 @@
+def rreplace(s, old, new, occurrence):
+    """Replace all occurrences except first"""
+    li = s.rsplit(old, occurrence)
+    return new.join(li)
+
+
+def fix_string(arg):
+    """Heuristics for OCR"""
+    arg = arg.replace("g", "0")
+    arg = arg.replace("To", "TD")
+    arg = arg.replace("TO", "TD")
+    arg = arg.replace("a", "3")
+    arg = arg.replace("o", "0")
+    arg = arg.replace("w", "V")
+    arg = arg.replace("h", "1")
+    arg = arg.replace("e", "G")
+    arg = arg.replace("z", "2")
+    if not arg.startswith("c"):
+        arg = arg.replace("c", "0")
+    if len(arg) <= 4 and len(arg) > 1:
+        if "I" in arg:
+            arg = arg.replace("I", "1")
+        elif "N" in arg:
+            arg = arg.replace("N", "1")
+        elif "Z" in arg:
+            arg = arg.replace("Z", "2")
+        elif arg.startswith("1"):
+            arg = arg.replace("1", "L", 1)
+        elif arg.endswith("T"):
+            arg = arg.replace(arg, arg[:-1] + "1", 1)
+        elif arg.endswith("D"):
+            arg = arg.replace(arg, arg[:-1] + "0", 1)
+        elif arg.endswith("A"):
+            arg = arg.replace(arg, arg[:-1] + "1", 1)
+        elif arg.endswith("B"):
+            arg = arg.replace(arg, arg[:-1] + "8", 1)
+        elif "T" in arg[3:]:
+            if arg.count("T") == 1:
+                arg = arg.replace("T", "1")
+            else:
+                arg = rreplace(arg, "T", "1", arg.count("T") - 1)
+        elif not arg.startswith("A"):
+            if arg.count("A") == 1:
+                arg = arg.replace("A", "4", 1)
+        elif not arg.startswith("T") and not arg.startswith("B"):
+            arg = arg.replace("B", "8", 1)
+
+    if len(arg) >= 4:
+        arg = arg.upper()
+        if "O" in arg:
+            arg = arg.replace("O", "0")
+        elif "B" in arg[3:]:
+            arg = arg.replace("B", "8")
+        elif "I" in arg[3:]:
+            arg = arg.replace("I", "1")
+        elif "E" in arg[3:]:
+            arg = arg.replace("E", "8")
+        elif "K" in arg[2:]:
+            arg = arg.replace("K", "1")
+        elif arg.startswith("5") or ("5" in arg[:2]):
+            arg = arg.replace("5", "S", 1)
+    return arg
