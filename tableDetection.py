96de81c0dc3b2d84ad05fce7cfc42ea7633bd90b
VisheshBansal <vishesh.bansal@gmail.com>
2021-09-22 17:49:25 +0530

    fix: detect fresher new slots

--- tableDetection.py
+++ tableDetection.py
@@ -140,7 +140,7 @@ def fetch_data(image):
                     if text[dash] == "-":
                         slot = text[:dash]
                         break
-            course_name_raw = re.findall(r"[A-z0-9]{3,6}[0-9]{1,4}", text)
+            course_name_raw = re.findall(r"[A-z0-9]{3,6}[0-9]{1,4}[A-z]{0,1}", text)
             if len(course_name_raw) > 1:
                 for i in course_name_raw:
                     if len(i) < 7:
@@ -197,12 +197,12 @@ def fetch_data(image):
 def fetch_text_timetable(text):
     data, slots = [], []
     slots += re.findall(
-        r"[A-Z]{1,3}[0-9]{1,2}[\D]{1}[A-Z]{3}[0-9]{4}[\D]{1}[A-Z]{2,3}[\D]{1}[A-Z]{2,4}[0-9]{2,4}[A-Z]{0,1}[\D]{1}[A-Z]{3}",
+        r"[A-Z]{1,3}[0-9]{1,2}[\D]{1}[A-Z]{3,4}[0-9]{3,4}[A-Z]{0,1}[\D]{1}[A-Z]{2,3}[\D]{1}[A-Z]{2,4}[0-9]{2,4}[A-Z]{0,1}[\D]{1}[A-Z]{3}",
         text,
     )
     for single_slot in slots:
         slot = re.findall(r"[A-Z]{1,3}[0-9]{1,2}\b", single_slot)[0]
-        course_name = re.findall(r"[A-Z]{3}[0-9]{4}\b", single_slot)[0]
+        course_name = re.findall(r"[A-Z]{3,4}[0-9]{3,4}[A-Z]{0,1}\b", single_slot)[0]
         course_code = re.findall(r"[ETH,SS,ELA,LO]{2,3}\b", single_slot)
         course_type = "Lab" if course_code[0] in ("ELA", "LO") else "Theory"
         venue = re.findall(r"[A-Z]{2,4}[0-9]{2,4}[A-Z]{0,1}\b", single_slot)[1]
