db3015c8a899272ad103ad11246a8c791ff1e6b8
Dhruv Shah <dhruvshahrds@gmail.com>
2023-08-20 01:21:11 +0530

    feat: root and ping endpoints

--- vitty-backend-api/api/initialize.go
+++ vitty-backend-api/api/initialize.go
@@ -7,6 +7,19 @@ import (
 )
 
 func InitializeApi(f *fiber.App) {
+	// Root endpoint
+	f.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(fiber.StatusOK).SendString("Welcome to VITTY API!🎉")
+	})
+
+	// Ping endpoint
+	f.Get("/ping", func(c *fiber.Ctx) error {
+		return c.Status(fiber.StatusOK).JSON(
+			fiber.Map{
+				"detail": "pong",
+			})
+	})
+
 	api := f.Group("/api")
 	v1.V1Handler(api)
 	v2.V2Handler(api)
