eee88e4e232b770ba98b463ce5e067850f901148
TheSilentSage <83112995+TheSilentSage@users.noreply.github.com>
2025-01-06 23:44:35 +0530

    feat: added option for users to hide timetable from friends
    * fix: merged slot for lab while fetching timetable
    
    * docs: updated postman documentation
    
    * feat: added option for users to hide timetable from friends
    
    ---------
    
    Co-authored-by: Dev Keshwani <dev.keshwani2022@vitstudent.ac.in>

--- vitty-backend-api/api/v2/userHandler.go
+++ vitty-backend-api/api/v2/userHandler.go
@@ -1,7 +1,9 @@
 package v2
 
 import (
+	"errors"
 	"fmt"
+	"log"
 
 	"github.com/GDGVIT/vitty-backend/vitty-backend-api/api/middleware"
 	"github.com/GDGVIT/vitty-backend/vitty-backend-api/api/serializers"
@@ -9,6 +11,7 @@ import (
 	"github.com/GDGVIT/vitty-backend/vitty-backend-api/internal/models"
 	"github.com/GDGVIT/vitty-backend/vitty-backend-api/internal/utils"
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
 )
 
 func userHandler(api fiber.Router) {
@@ -60,10 +63,29 @@ func getUser(c *fiber.Ctx) error {
 	fmt.Println("Friends", user.IsFriendsWith(request_user))
 
 	if (user.Username == request_user.Username) ||
-		(user.IsFriendsWith(request_user)) ||
 		(request_user.Role == "admin") {
 		return c.Status(fiber.StatusOK).JSON(serializers.UserSerializer(user, request_user))
 	}
+
+	if user.IsFriendsWith(request_user) {
+		err, isGhosted := request_user.IsGhosted(username)
+
+		if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+			log.Println(err)
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.ErrInternalServerError)
+
+		}
+
+		if isGhosted || errors.Is(err, gorm.ErrRecordNotFound) {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"code":  "1811",
+				"error": "",
+			})
+		}
+
+		return c.Status(fiber.StatusOK).JSON(serializers.UserSerializer(user, request_user))
+	}
+
 	return c.Status(fiber.StatusOK).JSON(serializers.UserCardSerializer(user, request_user))
 }
 
